7cbe15536f4bb47206209fc1c868ddaed498b9fa
genaroibc <bonavitaismael@gmail.com>
2023-07-13 23:17:08 -0300

    [DEV]: update deps and declare global interfaces

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import { ethers } from "ethers"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+
+declare global {
+  interface Window {
+    ethereum: ethers.providers.ExternalProvider
+  }
+}
